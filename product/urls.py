dab565ae24c80456ce256b0bccca6fa888eae520
Nora <alexnnn558@gmail.com>
2026-05-05 15:59:28 +0600

    first commit

---
+++ product/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('categories/', views.category_list),
+    path('categories/<int:id>/', views.category_detail),
+
+    path('products/', views.product_list),
+    path('products/<int:id>/', views.product_detail),
+
+    path('reviews/', views.review_list),
+    path('reviews/<int:id>/', views.review_detail),
+]
---
+++ product/views.py
@@ -0,0 +1,53 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category, Product, Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    data = CategorySerializer(categories, many=True).data
+    return Response(data=data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def category_detail(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = CategorySerializer(category, many=False).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    data = ProductSerializer(products, many=True).data
+    return Response(data=data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = ProductSerializer(product, many=False).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def review_list(request):
+    reviews = Review.objects.all()
+    data = ReviewSerializer(reviews, many=True).data
+    return Response(data=data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def review_detail(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(review, many=False).data
+    return Response(data=data)
